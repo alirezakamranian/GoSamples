0dddaf1364a982cb5a4b7410d3456db93ed7951c
AlirezaKamranian <123934502+alirezakamranian@users.noreply.github.com>
2024-11-24 19:56:30 +0330

    Feat:Add all go fundamentals

--- HelloWorld/HelloWorld.go
+++ HelloWorld/HelloWorld.go
@@ -1,5 +1,8 @@
 package main
-import ("fmt")
+
+import (
+	"fmt"
+)
 
 var vari = 12 
 
@@ -20,9 +23,84 @@ func main() {
 
 	fmt.Println(d)
 	sayhi();
+
+	if d == "HelloWorld"{
+		sayhi();
+	}else{
+		fmt.Println("else");
+	}
+
+	switch names[1]{
+
+	case "sina":
+		fmt.Println("sina")
+
+	case "mahomad":
+		fmt.Println("mahomad")
+
+	case "ali","dara":
+		fmt.Println("Baddies!!")
+
+	default:
+		fmt.Println("._.");
+	}
+
+	for i:=0 ; i<10 ; i++ {
+		fmt.Println(i)
+		if i>5{
+			break
+		}
+	}
+
+	for _,val := range names{
+		fmt.Println(val)
+	}
+
+	for idx,val := range names{
+		fmt.Println(idx,val)
+	}
+
+	response  := sayWordWithAllah("ya")
+
+	fmt.Println(response)
+
+	a,b := return10bomb()
+
+	println(a,b)
+
+    var person1 person 
+	person1.Name="ali"
+	person1.Age=19
+
+	var mapsample = make(map[int]string)
+	
+	mapsample[1]="ali"
+	mapsample[2]="lia"
+	
+	delete(mapsample,2)
+
+	val,exist := mapsample[1]
+
+	println(exist,val)
+}
+
+type person struct{
+	Age int
+	Name string
 }
 
 func sayhi(){
 	fmt.Println("hiiiiiiiiiiiiii")
+}
+
+func sayWordWithAllah(preallah string)string{
+	fmt.Println(preallah+" allah")
+	return "ok"
+}
+
+func return10bomb()(res int,txt string){
+	res=10
+	txt="bomb"
+	return
 }
 //Command to Run: "go run ./HelloWorld.go"
