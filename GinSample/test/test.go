27b64b0ccfaa968335d708e14bd709eafd9a9f23
AlirezaKamranian <123934502+alirezakamranian@users.noreply.github.com>
2024-11-27 13:16:42 +0330

    Feat:Add query binding to getuser

--- GinSample/test/test.go
+++ GinSample/test/test.go
@@ -1,60 +1,74 @@
+
 package main
 
 import (
+
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
+
 )
 
-type User struct {
-	gorm.Model
-	UserName string
-}
+	type User struct {
+		gorm.Model
+		UserName string
+	}
 
-type CreateUserDto struct{
-	UserName string
-}
+	type CreateUserDto struct {
+		UserName string
+	}
+
+	type Q struct {
+		Name     string    `form:"name"`
+	}
 
 func setupRouter() *gin.Engine {
 
-	db, err := gorm.Open(sqlserver.Open("sqlserver://sa:12230500o90P@127.0.0.1:1433?database=GormTest"), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	println(db.Name())
-
-	db.AutoMigrate(&User{})
-
-	r := gin.Default()
-
-	r.POST("/adduser",func(c *gin.Context){
-		var user CreateUserDto
-		
-		if err := c.ShouldBindJSON(&user); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
+		db, err := gorm.Open(sqlserver.Open("sqlserver://sa:12230500o90P@127.0.0.1:1433?database=GormTest"), &gorm.Config{})
+		if err != nil {
+			panic("failed to connect database")
 		}
 
-		db.Create(&User{UserName: user.UserName})
-		c.JSON(http.StatusOK, gin.H{"name": user.UserName})
-	})
+		println(db.Name())
 
-	r.GET("/getuser", func(c *gin.Context) {
-		var user User
-		db.Find(&user,user.UserName=="sina")
-		c.JSON(http.StatusOK, gin.H{"name":user.UserName,"Id":user.ID})
-	})
+		db.AutoMigrate(&User{})
 
-	r.GET("/getdetails", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"Ip": c.ClientIP(), "Name": "kami", "Url": c.Request.URL.Path})
-	})
-	return r
-}
+		r := gin.Default()
 
-func main() {
-	r := setupRouter()
-	r.Run("192.168.1.20:8080")
-}
+		r.POST("/adduser", func(c *gin.Context) {
+			var user CreateUserDto
+
+			if err := c.ShouldBindJSON(&user); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+
+			db.Create(&User{UserName: user.UserName})
+			c.JSON(http.StatusOK, gin.H{"name": user.UserName})
+		})
+
+		r.GET("/getuser", func(c *gin.Context) {
+			var user User
+			//db.First(&user)
+
+			var q Q
+
+			c.ShouldBind(&q)
+
+			db.Where("User_Name = ?", q.Name).Find(&user)
+
+			c.JSON(http.StatusOK, gin.H{"name": user.UserName, "Id": user.ID})
+		})
+
+		r.GET("/getdetails", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"Ip": c.ClientIP(), "Name": "kami", "Url": c.Request.URL.Path})
+		})
+		return r
+	}
+
+	func main() {
+		r := setupRouter()
+		r.Run("192.168.1.20:8080")
+	}
